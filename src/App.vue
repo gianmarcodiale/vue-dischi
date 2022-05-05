4e52da7adfa1959ec143254592f593dd5c3ba1fc
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-05 16:26:54 +0200

    add site header

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SiteHeader/>
+  </div>
+</template>
+
+<script>
+import SiteHeader from '@/components/SiteHeaderComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SiteHeader
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+</style>
---
+++ src/components/SiteHeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      <img src="@/assets/img/Spotify_logo.png" alt="" class="img-fluid">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'SiteHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+header {
+    background-color: $footerCardsColor;
+    padding: 1rem;
+    img {
+        width: 50px;
+    }
+}
+</style>
